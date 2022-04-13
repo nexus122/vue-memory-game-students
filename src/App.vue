bd813d71d104e92e52edaf6ca3c533b9de6fe48e
nexus122 <juan-pablo122@hotmail.es>
2022-04-13 17:40:30 +0200

    Cambios en el juego y restart

--- src/App.vue
+++ src/App.vue
@@ -9,34 +9,35 @@ import RadialProgress from "vue3-radial-progress";
 import { reactive } from "vue";
 
 // Iteración 1. Haced un console.log para ver el resultado. Sugerencia: cread una variable nueva normal y corriente
-
-shuffleArray(pokedex);
-
-let pokemon = pokedex.slice(0, 10);
-
-// usar .map adecuadamente sobre la variable 'pokemon'. Reasignar el resultado en 'pokemon' solo con los 3 campos requeridos.
-
-// Primera iteración: me quedo solo con el id y el nombre en inglés
-// Segunda iteración: Construyo la ruta a la imagen. Por ejemplo para el primer pokemon seria '/pokemons/001.png' , pero para el 10 sería '/pokemons/010.png'
-pokemon = pokemon.map((p) => {
-  return {
-    id: p.id,
-    name: p.name.english,
-    image: `/pokemons/${p.id.toString().padStart(3, "0")}.png`,
-  };
-});
+let pokemon = reactive([]);
+function obtenerPokemons() {
+  shuffleArray(pokedex);
+  pokemon = pokedex.slice(0, 10);
+  pokemon = pokemon.map((p) => {
+    return {
+      id: p.id,
+      name: p.name.english,
+      image: `/pokemons/${p.id.toString().padStart(3, "0")}.png`,
+    };
+  });
+}
+obtenerPokemons();
 
 const state = reactive({
   score: 0,
   totalMatches: 10,
-  tryClicks: 0
+  tryClicks: 0,
 });
 
 function checkCards(isMatch) {
   state.tryClicks++;
   if (isMatch) {
-    state.score++;    
+    state.score++;
   }
+}
+
+function restart(){
+  location.reload();
 }
 
 function shuffleArray(inputArray) {
@@ -46,11 +47,15 @@ function shuffleArray(inputArray) {
 
 <template>
   <h1>¡PokeMemory!</h1>
-  <br>
-  <header>    
-    <p class="header--info">Intentos: {{state.tryClicks}}</p>
-    <p class="header--info">Encontrados: {{state.score}}/{{state.totalMatches}}</p>
+  <br />
+  <header>
+    <p class="header--info">Intentos: {{ state.tryClicks }}</p>
+    <p class="header--info">
+      Encontrados: {{ state.score }}/{{ state.totalMatches }}
+    </p>
   </header>
+
+  <button @click="restart" v-if="state.score == state.totalMatches" class="btn">Restart</button>
 
   <main>
     <ChessBoard
@@ -63,7 +68,7 @@ function shuffleArray(inputArray) {
 
 <style>
 @import "./assets/base.css";
-@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
 
 header {
   line-height: 1.5;
@@ -71,23 +76,37 @@ header {
   flex-wrap: wrap;
   justify-content: space-around;
   width: 100%;
-  font-family: 'Press Start 2P', cursive;
+  font-family: "Press Start 2P", cursive;
   text-align: center;
 }
-h1{
-  font-family: 'Press Start 2P', cursive;
+h1 {
+  font-family: "Press Start 2P", cursive;
 }
-main{
+
+main {
   margin-top: 2em;
 }
+
+.btn {
+  padding: 1em;
+  font-size: 1em;
+  border: 0px;
+  background-color: var(--vt-c-indigo);
+  color: white;
+  cursor: pointer;
+  border-radius: 1em;
+  min-width: 200px;
+  font-family: "Press Start 2P", cursive;
+}
+
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
-.header--info{
+.header--info {
   font-size: 2em;
 }
-.heder--score{
+.heder--score {
   font-size: 1em;
 }
 
